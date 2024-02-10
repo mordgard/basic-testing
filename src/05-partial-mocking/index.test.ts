7d8cd2ddea768139492d5f8980ab690c3ec2f4dc
Mikhail Sharakou <msharakov@gmail.com>
2024-02-10 11:12:46 +0300

    add part mocking

--- src/05-partial-mocking/index.test.ts
+++ src/05-partial-mocking/index.test.ts
@@ -1,8 +1,18 @@
 // Uncomment the code below and write your tests
-// import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
+import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
 
 jest.mock('./index', () => {
-  // const originalModule = jest.requireActual<typeof import('./index')>('./index');
+  const originalModule =
+    jest.requireActual<typeof import('./index')>('./index');
+
+  const mocked = jest.fn();
+
+  return {
+    ...originalModule,
+    mockOne: mocked,
+    mockTwo: mocked,
+    mockThree: mocked,
+  };
 });
 
 describe('partial mocking', () => {
@@ -11,10 +21,12 @@ describe('partial mocking', () => {
   });
 
   test('mockOne, mockTwo, mockThree should not log into console', () => {
-    // Write your test here
+    mockOne();
+    mockTwo();
+    mockThree();
   });
 
   test('unmockedFunction should log into console', () => {
-    // Write your test here
+    unmockedFunction();
   });
 });
